bcdd44697d7974c1a9cf2429de9882f1ae37db07
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-23 22:18:22 +0900

    # 이슈 랜더링에 필요한 dto 작성

---
+++ src/main/java/org/nmfw/foodietree/domain/customer/dto/resp/CustomerIssueDetailDto.java
@@ -0,0 +1,25 @@
+package org.nmfw.foodietree.domain.customer.dto.resp;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.nmfw.foodietree.domain.customer.entity.value.IssueCategory;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Slf4j
+@Builder
+public class CustomerIssueDetailDto {
+    private String customerId;
+    private int reservationId;
+    private String storeName;
+    private String nickname;
+    private IssueCategory issueCategory;
+    private LocalDateTime issueCompleteAt;
+    private String issueText;
+    private LocalDateTime cancelIssueAt;
+}
