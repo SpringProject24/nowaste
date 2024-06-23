e7fc334ff6f3521776b0210efa1af1692b6de792
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-23 09:19:14 +0900

    # Transactional 추가

--- src/test/java/org/nmfw/foodietree/domain/customer/mapper/CustomerMyPageMapperTest.java
+++ src/test/java/org/nmfw/foodietree/domain/customer/mapper/CustomerMyPageMapperTest.java
@@ -7,13 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-//@Transactional
+@Transactional
 class CustomerMyPageMapperTest {
 
     private static final Logger log = LoggerFactory.getLogger(CustomerMyPageMapperTest.class);
