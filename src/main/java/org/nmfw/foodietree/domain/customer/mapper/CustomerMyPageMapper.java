909dded8a4789b9bf0da05e588fee58dc75252fd
Jeong-in <162551171+munginisfree@users.noreply.github.com>
2024-06-24 01:50:31 +0900

    # findFavStore mapper에 작성

--- src/main/java/org/nmfw/foodietree/domain/customer/mapper/CustomerMyPageMapper.java
+++ src/main/java/org/nmfw/foodietree/domain/customer/mapper/CustomerMyPageMapper.java
@@ -2,7 +2,7 @@ package org.nmfw.foodietree.domain.customer.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.nmfw.foodietree.domain.customer.dto.resp.CustomerIssueDetailDto;
+import org.nmfw.foodietree.domain.customer.dto.resp.CustomerFavStoreDto;
 import org.nmfw.foodietree.domain.customer.dto.resp.CustomerMyPageDto;
 import org.nmfw.foodietree.domain.customer.entity.CustomerIssues;
 import org.nmfw.foodietree.domain.customer.entity.ReservationDetail;
@@ -20,6 +20,9 @@ public interface CustomerMyPageMapper {
 
     // 회원 선호 음식 조회
     List<String> findPreferenceFoods(@Param("customerId") String customerId);
+
+    // customer 좋아요 표시한 가게 조회
+    List<CustomerFavStoreDto> findFavStore(@Param("customerId") String customerId);
 
     // 회원 예약 내역 조회
     // ReservationDetail로 반환받음
