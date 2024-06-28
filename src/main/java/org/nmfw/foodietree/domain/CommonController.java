dfb546168c23fd55d9e0cd7fffe20562915b23d3
hoseong511 <thdghtjd115@naver.com>
2024-06-28 23:45:51 +0900

    WIP mypage css

--- src/main/java/org/nmfw/foodietree/domain/CommonController.java
+++ src/main/java/org/nmfw/foodietree/domain/CommonController.java
@@ -32,6 +32,33 @@ public class CommonController {
         return "sign-in";
     }
 
+    @GetMapping("/customer/mypage-test")
+    public String myPageMain(
+                            Model model
+                            , HttpServletRequest request
+                            , HttpServletResponse response) {
+        log.info("/customer/mypage-main POST");
+
+        // 1. 로그인 되어있는 회원 아이디 가져오기
+        String customerId = "test@gmail.com";
+        // 2. 데이터베이스에서 해당 회원 데이터 조회하기
+        CustomerMyPageDto customerMyPageDto = customerMyPageService.getCustomerInfo(customerId,
+            request, response);
+
+        List<MyPageReservationDetailDto> myPageReservationDetailDto = customerMyPageService.getReservationInfo(
+            customerId);
+
+        List<CustomerIssueDetailDto> customerIssueDetailDto = customerMyPageService.getCustomerIssues(
+            customerId);
+        // 3. JSP파일에 조회한 데이터 보내기
+        model.addAttribute("customerMyPageDto", customerMyPageDto);
+        model.addAttribute("reservations", myPageReservationDetailDto);
+        model.addAttribute("issues", customerIssueDetailDto);
+        return "customer/mypage";
+    }
+
+
+
     @GetMapping("/customer/mypage-edit")
     public String myPageEdit(
                             Model model
